bdd570d079e8edef12a3392908c8ac48408e27fe
Victor Whyte <victordavidwhyte@gmail.com>
2023-07-12 19:35:12 +0100

    feat: getallreports endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, Param } from '@nestjs/common';
+import { data, ReportType } from './data';
 
 @Controller('report/:type')
 export class AppController {
   @Get()
-  getAllReports() {
-    return [];
+  getAllReports(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report.filter((report) => report.type === reportType);
   }
 
   @Get(':id')
