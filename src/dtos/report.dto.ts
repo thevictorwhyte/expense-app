06b926b5c20e0c9f72800f7083b04ff74d4f1ff4
Victor Whyte <victordavidwhyte@gmail.com>
2023-07-13 16:12:47 +0100

    feat: setup create report dto and global pipes validation

---
+++ src/dtos/report.dto.ts
@@ -0,0 +1,11 @@
+import { IsNumber, IsPositive, IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateReportDto {
+  @IsNumber()
+  @IsPositive()
+  amount: number;
+
+  @IsString()
+  @IsNotEmpty()
+  source: string;
+}
