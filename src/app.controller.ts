ce95663aa6956e2966873e1ecb46d40de727f5e1
Victor Whyte <victordavidwhyte@gmail.com>
2023-07-13 10:53:39 +0100

    refactor: move getAllReports business logic inside of app service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,14 +10,17 @@ import {
 } from '@nestjs/common';
 import { data, ReportType, Report } from './data';
 import { v4 as uuid } from 'uuid';
+import { AppService } from './app.service';
 
 @Controller('report/:type')
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get()
   getAllReports(@Param('type') type: string) {
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    return data.report.filter((report) => report.type === reportType);
+    return this.appService.getAllReports(reportType);
   }
 
   @Get(':id')
