75f540664a62c0fe87c475f77b4e87d57d796620
Victor Whyte <victordavidwhyte@gmail.com>
2023-07-14 08:35:08 +0100

    feat: transform return values for report

--- src/dtos/report.dto.ts
+++ src/dtos/report.dto.ts
@@ -34,6 +34,13 @@ export class ReportResponseDto {
   id: string;
   source: string;
   amount: number;
+
+  @Expose({ name: 'createdAt' })
+  transformCreatedAt() {
+    return this.created_at;
+  }
+
+  @Exclude()
   created_at: Date;
 
   @Exclude()
