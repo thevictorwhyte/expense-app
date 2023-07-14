b44dff3b2c053b33c39417aa022bd8de9a9fafc6
Victor Whyte <victordavidwhyte@gmail.com>
2023-07-14 16:54:11 +0100

    feat: write custom interceptor

---
+++ src/custom.intercepter.ts
@@ -0,0 +1,19 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { map } from 'rxjs';
+
+export class CustomInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, handler: CallHandler) {
+    return handler.handle().pipe(
+      map((data) => {
+        const response = {
+          ...data,
+          createdAt: data.created_at,
+        };
+
+        delete response.updated_at;
+        delete response.created_at;
+        return response;
+      }),
+    );
+  }
+}
