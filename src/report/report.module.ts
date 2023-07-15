25c60dec089f3292a7b17a755d002a15ba9095a1
Victor Whyte <victordavidwhyte@gmail.com>
2023-07-15 13:51:16 +0100

    refactor: create seperate entity for report

---
+++ src/report/report.module.ts
@@ -0,0 +1,16 @@
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { ReportController } from './report.controller';
+import { ReportService } from './report.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+
+@Module({
+  controllers: [ReportController],
+  providers: [
+    ReportService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
+})
+export class ReportModule {}
